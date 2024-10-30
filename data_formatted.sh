a5f02564624b5d589af57ef6e39b61a79e813037
luis-ntonio <luis.gutierrez.g@utec.edu.pe>
2024-10-30 08:54:45 -0500

    a

--- data_formatted.sh
+++ data_formatted.sh
@@ -5,5 +5,5 @@ options=("p" "d" "t")
 
 # Loop through each option and call data_formatted.py
 for option in "${options[@]}"; do
-    python3 data_formatted.py --option "$option"
+    python data_formatted.py --option "$option"
 done
